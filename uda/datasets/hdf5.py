085ade0a9a83f04774126bd05ec9d8c3fc0b0b71
Ch4nYH <hfercxx@hotmail.com>
2020-07-23 22:55:33 +0800

    update

--- uda/datasets/hdf5.py
+++ uda/datasets/hdf5.py
@@ -5,38 +5,38 @@ from torch.utils.data import Dataset
 from tqdm import tqdm
 
 class HDF5Dataset(Dataset):
-  """ JHU Dataset """
+  	""" JHU Dataset """
 
-  def __init__(self, list_file, root_dir, transform=None, read_label=True, need_non_zero_label = True, is_binary = False):
-	"""
-	Args:
-	  list_file (string) : List of the image files
-	  root_dir (string) : Directory of images
-	  transform (callable, optional) : Optional transform to be applied on a sample
-	"""
-	self.image_list = open(list_file).readlines()
-	self.image_list = [os.path.basename(line.strip()) for line in self.image_list]
-	self.image_list = [line for line in self.image_list if line.endswith('.h5')]
-	
-	self.root_dir = root_dir
-	self.read_label = read_label
-	self.transform = transform
-	if need_non_zero_label:
-		self.temp = []
-		pbar = tqdm(self.image_list)
-		for img in pbar:
-			img_name = os.path.join(self.root_dir, img)
-			data = h5py.File(img_name, 'r')
-			label = np.array(data['label'])
-			if is_binary and label.max() > 1:
-				label[label > 1] = 0
-			if len(np.nonzero(label)[0]) * len(np.nonzero(label)[1]) * len(np.nonzero(label)[2]) > 0:
-				self.temp.append(img)
-			else:
-				pbar.set_description("Removing {}".format(img))
-		self.image_list = self.temp
-		print('read {} images'.format(len(self.image_list)))
-	
+  	def __init__(self, list_file, root_dir, transform=None, read_label=True, need_non_zero_label = True, is_binary = False):
+		"""
+		Args:
+		  list_file (string) : List of the image files
+		  root_dir (string) : Directory of images
+		  transform (callable, optional) : Optional transform to be applied on a sample
+		"""
+		self.image_list = open(list_file).readlines()
+		self.image_list = [os.path.basename(line.strip()) for line in self.image_list]
+		self.image_list = [line for line in self.image_list if line.endswith('.h5')]
+		
+		self.root_dir = root_dir
+		self.read_label = read_label
+		self.transform = transform
+		if need_non_zero_label:
+			self.temp = []
+			pbar = tqdm(self.image_list)
+			for img in pbar:
+				img_name = os.path.join(self.root_dir, img)
+				data = h5py.File(img_name, 'r')
+				label = np.array(data['label'])
+				if is_binary and label.max() > 1:
+					label[label > 1] = 0
+				if len(np.nonzero(label)[0]) * len(np.nonzero(label)[1]) * len(np.nonzero(label)[2]) > 0:
+					self.temp.append(img)
+				else:
+					pbar.set_description("Removing {}".format(img))
+			self.image_list = self.temp
+			print('read {} images'.format(len(self.image_list)))
+		
 	def __len__(self):
 		return len(self.image_list)
 
