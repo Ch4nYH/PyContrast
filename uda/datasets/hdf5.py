87c15cc44ca5fc790d907d0ed61859eb1a319f4e
Ch4nYH <hfercxx@hotmail.com>
2020-07-23 22:35:01 +0800

    update

--- uda/datasets/hdf5.py
+++ uda/datasets/hdf5.py
@@ -22,35 +22,36 @@ class HDF5Dataset(Dataset):
 	self.read_label = read_label
 	self.transform = transform
 	if need_non_zero_label:
-	  self.temp = []
-	  pbar = tqdm(self.image_list)
-	  for img in pbar:
-		img_name = os.path.join(self.root_dir, img)
-		data = h5py.File(img_name, 'r')
-		label = np.array(data['label'])
-		if is_binary and label.max() > 1:
-		  label[label > 1] = 0
-		if len(np.nonzero(label)[0]) * len(np.nonzero(label)[1]) * len(np.nonzero(label)[2]) > 0:
-		  self.temp.append(img)
-		else:
-		  pbar.set_description("Removing {}".format(img))
-	  self.image_list = self.temp
-	print('read {} images'.format(len(self.image_list)))
+		self.temp = []
+		pbar = tqdm(self.image_list)
+		for img in pbar:
+			img_name = os.path.join(self.root_dir, img)
+			data = h5py.File(img_name, 'r')
+			label = np.array(data['label'])
+			if is_binary and label.max() > 1:
+				label[label > 1] = 0
+			if len(np.nonzero(label)[0]) * len(np.nonzero(label)[1]) * len(np.nonzero(label)[2]) > 0:
+				self.temp.append(img)
+			else:
+				pbar.set_description("Removing {}".format(img))
+		self.image_list = self.temp
+		print('read {} images'.format(len(self.image_list)))
 	
-  def __len__(self):
-	return len(self.image_list)
+	def __len__(self):
+		return len(self.image_list)
 
-  def __getitem__(self, idx):
-	img_name = os.path.join(self.root_dir, self.image_list[idx])
-	data = h5py.File(img_name, 'r')
-	image = np.array(data['image'], dtype=np.float32) 
-	if self.read_label:
-	  label = np.array(data['label'])
-	  sample = {'image': image, 'label': label}
-	else:
-	  sample = {'image': image}
-	data.close()
+	def __getitem__(self, idx):
+		img_name = os.path.join(self.root_dir, self.image_list[idx])
+		data = h5py.File(img_name, 'r')
+		image = np.array(data['image'], dtype=np.float32) 
+		if self.read_label:
+			label = np.array(data['label'])
+			sample = {'image': image, 'label': label}
+		else:
+			sample = {'image': image}
+		data.close()
 
-	if self.transform:
-	  sample = self.transform(sample)
-	return sample
+		if self.transform:
+			sample = self.transform(sample)
+		
+		return sample
