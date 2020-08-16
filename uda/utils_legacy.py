54872721018bea0a15e8e0687794646cf6a8d912
Ch4nYH <hfercxx@hotmail.com>
2020-08-16 18:03:08 +0800

    update

--- uda/utils_legacy.py
+++ uda/utils_legacy.py
@@ -89,10 +89,10 @@ def color_map(N=256, normalized=False):
 		r = g = b = 0
 		c = i
 		for j in range(8):
-		r = r | (bitget(c, 0) << 7 - j)
-		g = g | (bitget(c, 1) << 7 - j)
-		b = b | (bitget(c, 2) << 7 - j)
-		c = c >> 3
+			r = r | (bitget(c, 0) << 7 - j)
+			g = g | (bitget(c, 1) << 7 - j)
+			b = b | (bitget(c, 2) << 7 - j)
+			c = c >> 3
 
 		cmap[i] = np.array([r, g, b])
 
