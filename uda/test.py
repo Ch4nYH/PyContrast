c9cb918250ee49b0970a0be509a87afc0720accd
Ch4nYH <hfercxx@hotmail.com>
2020-10-10 17:17:05 +0800

    update

--- uda/test.py
+++ uda/test.py
@@ -100,7 +100,7 @@ if __name__ == "__main__":
             shape[0] *= 3
             image = image.reshape((1, 1,) + image.shape)
             image = F.interpolate(image, size=(1, 1,) + tuple(shape), mode='trilinear')
-            image = image[0,0,:,;,:]
+            image = image[0,0,:,:,:]
         # time_map P= np.zeros(image.shape).astype(np.float32)
         for x in range(0, sx):
             xs = stride * x
