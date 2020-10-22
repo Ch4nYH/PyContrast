96b423a5943d79d2bb96054a785dee49132a9e1c
Ch4nYH <hfercxx@hotmail.com>
2020-10-22 17:31:12 +0800

    update

--- uda/parallel_functions.py
+++ uda/parallel_functions.py
@@ -21,22 +21,23 @@ def train(model, model_ema, loader, optimizer, logger, saver, args, epoch, contr
 		volume = volume.view((-1,) + volume.shape[2:])
 		volume2 = batch['image_2'].cuda(args.local_rank, non_blocking = True)
 		volume2 = volume2.view((-1,) + volume2.shape[2:])
-	
-		q = model(volume, pretrain=True)
-		with torch.no_grad():
-			k = model_ema(volume2, pretrain=True)
+		
+		if epoch > args.turnon:
+			q = model(volume, pretrain=True)
+			with torch.no_grad():
+				k = model_ema(volume2, pretrain=True)
 
-		output = contrast(q, k, all_k=None)
-		losses, accuracies = compute_loss_accuracy(
-						logits=output[:-1], target=output[-1],
-						criterion=criterion)
-		optimizer.zero_grad()
-		if not args.increasing_coef:
-			(losses[0] * args.coef).backward()
-		else:
-			(losses[0] * epoch / args.epochs * args.coef).backward()
+			output = contrast(q, k, all_k=None)
+			losses, accuracies = compute_loss_accuracy(
+							logits=output[:-1], target=output[-1],
+							criterion=criterion)
+			optimizer.zero_grad()
+			if not args.increasing_coef:
+				(losses[0] * args.coef).backward()
+			else:
+				(losses[0] * epoch / args.epochs * args.coef).backward()
 
-		optimizer.step()
+			optimizer.step()
 
 		label  = batch['label'].cuda(args.local_rank)
 		label  = label.view((-1,) + label.shape[2:])
