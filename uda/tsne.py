f0eea51fb8d204fb73f26e247ff23d9d9f542ed4
Ch4nYH <hfercxx@hotmail.com>
2020-10-30 19:35:54 +0800

    update

--- uda/tsne.py
+++ uda/tsne.py
@@ -7,29 +7,20 @@ from tqdm import tqdm
 from utils.utils import dice, Logger, Saver, adjust_learning_rate
 from config import parse_args
 from datetime import datetime
-from parallel_functions import train, validate
+from functions import train, validate
 from datasets.paths import get_paths
 from datasets.hdf5 import HDF5Dataset
 from datasets.dataset import build_dataset
 
 from torch.utils.data import DataLoader
-from models.vnet_parallel import VNet
+from models.vnet import VNet
 from torch.nn.parallel import DistributedDataParallel as DDP
-from models.mem_moco import RGBMoCo
 
 def main():
 
     args = parse_args()
-    if args.turnon < 0:
-        args.pretrain = True
-    else:
-        args.pretrain = False
+    args.pretrain = True
     print("Using GPU: {}".format(args.local_rank))
-    root_path = 'exps/exp_{}'.format(args.exp)
-    if args.local_rank == 0 and not os.path.exists(root_path):
-        os.mkdir(root_path)
-        os.mkdir(os.path.join(root_path, "log"))
-        os.mkdir(os.path.join(root_path, "model"))
     
     base_lr = args.lr  # base learning rate
     batch_size = 1
@@ -69,41 +60,28 @@ def main():
     model = VNet(args.n_channels, args.n_classes, input_size = 64, pretrain = True).cuda(args.local_rank)
     model_ema = VNet(args.n_channels, args.n_classes, input_size = 64, pretrain = True).cuda(args.local_rank)
     optimizer = torch.optim.SGD(model.parameters(), lr = args.lr, momentum=0.9, weight_decay=0.0005)
-    if args.world_size > 1:
-        model = DDP(model, device_ids=[args.local_rank], output_device=args.local_rank, find_unused_parameters=True)
+    #scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.7)
+    parallel_state_dict = torch.load(args.load_path)['checkpoint']
+    new_state_dict = {}
+    for (key, value) in parallel_state_dict:
+        new_state_dict[key[:6]] = value
 
-    model.train()
-    model_ema = DDP(model_ema, device_ids=[args.local_rank], output_device=args.local_rank, find_unused_parameters=True)
-    model_ema.load_state_dict(model.state_dict())
+    model.load_state_dict(new_state_dict)
+    model.eval()
     print("Loaded weights")
-
-    logger = Logger(root_path)
-    saver = Saver(root_path)
-    contrast = RGBMoCo(128, K = 4096).cuda(args.local_rank)
-    criterion = torch.nn.CrossEntropyLoss()
-
-    flag = False
-    for epoch in range(args.start_epoch, args.epochs):
-        train_sampler.set_epoch(epoch)
-        train(model, model_ema, train_loader, optimizer, logger, saver, args, epoch, contrast, criterion)
-        validate(model_ema, val_loader, optimizer, logger, saver, args, epoch)
-        adjust_learning_rate(args, optimizer, epoch)
-        if not flag and args.turnon > 0 and epoch >= args.turnon:
-            args.pretrain = True
-            train_dataset, val_dataset = build_dataset(args)
-            train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset, num_replicas = len(args.gpu.split(",")), rank = args.local_rank)
-            train_loader = torch.utils.data.DataLoader(
-                train_dataset, batch_size=args.batch_size, 
-                shuffle=(train_sampler is None),
-                sampler = train_sampler,
-                num_workers=args.num_workers, pin_memory=True)
     
-            val_loader = torch.utils.data.DataLoader(
-                val_dataset, batch_size=1, 
-                num_workers=args.num_workers, pin_memory=True)
-            
-            flag = True
-        
 
-if __name__ == '__main__':
-    main()
+    features = []
+    for i, batch in enumerate(train_loader):
+        volume = batch['image'].cuda(args.local_rank, non_blocking = True)
+        volume = volume.view((-1,) + volume.shape[2:])
+
+        with torch.no_grad():
+            q = model(volume, pretrain=True)
+
+        features.append(q)
+
+    features = torch.cat(features, 0)
+
+    print(features.shape)
+
