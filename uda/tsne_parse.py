52c9e0c3e19195eb304c8401cef4ed1c376b5ade
Ch4nYH <hfercxx@hotmail.com>
2020-10-30 20:09:31 +0800

    update

--- uda/tsne_parse.py
+++ uda/tsne_parse.py
@@ -4,5 +4,5 @@ import pickle
 tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
 features = pickle.load(open('features.pkl', 'rb'))
 low_dim_embs = tsne.fit_transform(features)
-target = np.array([1,2,3,4]).repeat(features.shape[0] // 4, 1)
+target = np.array([1,2,3,4]).repeat(features.shape[0] // 4)
 
