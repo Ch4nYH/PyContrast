4859a453e4c2038722ddd8185776b51d5e0eec61
Ch4nYH <hfercxx@hotmail.com>
2020-10-31 09:29:48 +0800

    update

--- uda/tsne_parse.py
+++ uda/tsne_parse.py
@@ -9,14 +9,14 @@ def plot_with_labels(lowDWeights, labels):
     X, Y = lowDWeights[:, 0], lowDWeights[:, 1]
     for x, y, s in zip(X, Y, labels):
         c = cm.rainbow(int(255 * s / 9));
-        plt.text(x, y, s, backgroundcolor=c, fontsize=9)
+        plt.plot(x, y, color=c)
     plt.xlim(X.min(), X.max());
     plt.ylim(Y.min(), Y.max());
     plt.title('Visualize last layer')
     plt.show()
     plt.savefig("1.jpg")
 
-    
+
 tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
 features = pickle.load(open('features.pkl', 'rb'))
 low_dim_embs = tsne.fit_transform(features)
