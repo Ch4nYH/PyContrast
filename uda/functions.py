032a9919d3789932f5c46eaf41cfbf964c3045a2
Ch4nYH <hfercxx@hotmail.com>
2021-02-10 13:53:25 +0800

    update

--- uda/functions.py
+++ uda/functions.py
@@ -10,9 +10,9 @@ def train(model, loader, optimizer, logger, args, epoch, print_freq = 10):
 	model.train()
 	for i, batch in enumerate(loader):
 		index = batch['index']
-		volume = batch['image'].cuda(args.local_rank)
+		volume = batch['image'].cuda()
 		volume = volume.view((-1,) + volume.shape[2:])
-		label  = batch['label'].cuda(args.local_rank)
+		label  = batch['label'].cuda()
 		label  = label.view((-1,) + label.shape[2:])
 		output, _ = model(volume)
 		loss = cross_entropy_3d(output, label)
@@ -27,7 +27,7 @@ def train(model, loader, optimizer, logger, args, epoch, print_freq = 10):
 		dices.append(d)
 		losses.append(loss)
 		losses.append(loss.detach().cpu().item())
-		if i % print_freq == 0 and args.local_rank == 0:
+		if i % print_freq == 0:
 			tqdm.write('[Epoch {}, {}/{}] loss: {}, dice: {}'.format(epoch, i, len(loader), loss.detach().cpu().item(), d))
 
 			logger.log("train/loss", loss)
--- uda/pretrain.py
+++ uda/pretrain.py
@@ -22,13 +22,6 @@ apex = False
 args = parse_args()
 args.pretrain = True
 
-os.environ['MASTER_PORT'] = args.port
-torch.cuda.set_device(args.local_rank)
-torch.distributed.init_process_group(
-	'nccl',
-    init_method='env://'
-)
-device = torch.device('cuda:{}'.format(args.local_rank))
 root_path = 'exps/exp{}'.format(args.exp)
 if not os.path.exists(root_path):
 	os.mkdir(root_path)
@@ -37,36 +30,33 @@ if not os.path.exists(root_path):
 
 base_lr = args.lr  # base learning rate
 train_dataset, val_dataset = build_dataset(args.dataset, args.data_root, args.train_list, sampling = args.sampling)
-train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset, num_replicas = len(args.gpu.split(',')), rank = args.local_rank)
     
 train_loader = torch.utils.data.DataLoader(
     train_dataset, batch_size=args.batch_size, 
     shuffle=False,
-    sampler = train_sampler,
     num_workers=args.num_workers, pin_memory=True, drop_last = True)
     
 val_loader = torch.utils.data.DataLoader(
     val_dataset, batch_size=1, 
     num_workers=args.num_workers, pin_memory=True)
-model = VNet(args.n_channels, args.n_classes, input_size = 64, pretrain = True).cuda(args.local_rank)
-model_ema = VNet(args.n_channels, args.n_classes, input_size = 64, pretrain = True).cuda(args.local_rank)
+model = VNet(args.n_channels, args.n_classes, input_size = 64, pretrain = True).cuda()
+model_ema = VNet(args.n_channels, args.n_classes, input_size = 64, pretrain = True).cuda()
 
 
 optimizer = torch.optim.SGD(model.parameters(), lr = args.lr, momentum=0.9, weight_decay=0.0005)
-model = DDP(model, device_ids=[args.local_rank], output_device=args.local_rank, find_unused_parameters=True)
-model_ema = DDP(model_ema, device_ids=[args.local_rank], output_device=args.local_rank, find_unused_parameters=True)
+model = torch.nn.DataParllel(model)
+model_ema = torch.nn.DataParllel(model_ema)
 model_ema.load_state_dict(model.state_dict())
 print("Model Initialized")
 logger = Logger(root_path)
 saver = Saver(root_path, save_freq = args.save_freq)
 if args.sampling == 'default':
-    contrast = RGBMoCo(128, K = 4096, T = args.temperature).cuda(args.local_rank)
+    contrast = RGBMoCo(128, K = 4096, T = args.temperature).cuda()
 elif args.sampling == 'layerwise':
-    contrast = RGBMoCoNew(128, K = 4096, T = args.temperature).cuda(args.local_rank)
+    contrast = RGBMoCoNew(128, K = 4096, T = args.temperature).cuda()
 else:
     raise ValueError("unsupported sampling method")
 criterion = torch.nn.CrossEntropyLoss()
 for epoch in range(args.start_epoch, args.epochs):
-	train_sampler.set_epoch(epoch)
 	pretrain(model, model_ema, train_loader, optimizer, logger, saver, args, epoch, contrast, criterion)
 	adjust_learning_rate(args, optimizer, epoch)
